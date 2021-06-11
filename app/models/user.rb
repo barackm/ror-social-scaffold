f3b265d47a73fea6e3ad7e508a44d86d12b57314
Barack <barackmukelenga100@gmail.com>
2021-06-11 14:47:44 +0200

    Add relationship betweet user and friendship

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,7 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  has_many :friendships  
+  has_many :received_requests, class_name:"Friendship", foreign_key: "friend_id"
 end
