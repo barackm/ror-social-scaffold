e73768e2406de15b5c629fcf2112df26d62d256a
Barack <barackmukelenga100@gmail.com>
2021-06-14 17:09:53 +0200

    Fix css problems, and the bug in the friendship helper that was preventing users from deleting their friends

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -2,7 +2,7 @@ module FriendshipsHelper
   def check_buttons(user)
     if user.id == current_user.id
       ''
-    elsif Friendship.friend_with?(current_user.id, user.id)
+    elsif current_user.friends.any? {|f| f.id == user.id || f.id == user.id}
       link_to('Remove', user_friendships_path(user), method: :delete, class: 'control-btn remove')
     elsif current_user.pending_sent_friendship_requests.any? { |f| f.id == user.id }
       link_to('Pending', user_friendships_request_path(user), method: :delete, class: 'control-btn pending')
