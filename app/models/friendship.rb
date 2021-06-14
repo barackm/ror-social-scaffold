6a7643a495644f2c20d7a321fb5328d249eb32fc
Barack <barackmukelenga100@gmail.com>
2021-06-14 12:58:37 +0200

    Fix bug in friendship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,12 +3,11 @@ class Friendship < ApplicationRecord
 
   belongs_to :user
   belongs_to :friend, class_name: 'User', foreign_key: 'friend_id'
-
+  
   validates :status, presence: true
 
   private
 
-  
   def friend_with?(user1_id, user2_id)
     "we are friends"
   end
